bddc82270033f834a91a368b6e47895520a2a7a8
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-03-04 11:59:53 +0300

    Added Stop Loss to TSL cBot

---
+++ Robots/Stop Loss To TSL/Stop Loss To TSL/Stop Loss To TSL.cs
@@ -0,0 +1,28 @@
+﻿using cAlgo.API;
+using System;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// Changes current symbol positions normal stop loss to trailing stop loss once it reached x amount of pips in profit
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class StopLossToTSL : Robot
+    {
+        [Parameter("Pips Profit", DefaultValue = 20)]
+        public double PipsProfit { get; set; }
+
+        protected override void OnTick()
+        {
+            foreach (var position in Positions)
+            {
+                if (!position.SymbolName.Equals(SymbolName, StringComparison.OrdinalIgnoreCase)
+                    || position.HasTrailingStop
+                    || !position.StopLoss.HasValue
+                    || position.Pips < PipsProfit) continue;
+
+                ModifyPosition(position, position.StopLoss, position.TakeProfit, true, position.StopLossTriggerMethod);
+            }
+        }
+    }
+}
