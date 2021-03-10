0333c7fa140f7df4c561cbbec2d45bf6f4e01423
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-03-10 12:06:44 +0300

    Added chart icon sample

---
+++ Indicators/Chart Icon Sample/Chart Icon Sample/Chart Icon Sample.cs
@@ -0,0 +1,32 @@
+﻿using cAlgo.API;
+
+namespace cAlgo
+{
+    /// <summary>
+    /// This sample indicator shows how to use Chart.DrawIcon method to draw icons
+    /// </summary>
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class ChartIconSample : Indicator
+    {
+        protected override void Initialize()
+        {
+            for (int i = Chart.FirstVisibleBarIndex; i <= Chart.LastVisibleBarIndex; i++)
+            {
+                var iconName = string.Format("Icon_{0}", i);
+
+                if (Bars.ClosePrices[i] > Bars.OpenPrices[i])
+                {
+                    Chart.DrawIcon(iconName, ChartIconType.UpArrow, i, Bars.LowPrices[i], Color.Green);
+                }
+                else
+                {
+                    Chart.DrawIcon(iconName, ChartIconType.DownArrow, i, Bars.HighPrices[i], Color.Red);
+                }
+            }
+        }
+
+        public override void Calculate(int index)
+        {
+        }
+    }
+}
