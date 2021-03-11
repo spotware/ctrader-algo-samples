1428d5a30f5461c62ae27f37b0abecbd3b7f1862
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-03-11 11:19:54 +0300

    Added chart vertical line sample

---
+++ Indicators/Chart Vertical Line Sample/Chart Vertical Line Sample/Chart Vertical Line Sample.cs
@@ -0,0 +1,22 @@
+﻿using cAlgo.API;
+
+namespace cAlgo
+{
+    /// <summary>
+    /// This sample shows how to draw a vertical line on chart by using Chart.DrawVertical line method
+    /// </summary>
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class ChartVerticalLineSample : Indicator
+    {
+        protected override void Initialize()
+        {
+            var verticalLine = Chart.DrawVerticalLine("vertical_line", Chart.LastVisibleBarIndex, Color.Red, 2, LineStyle.DotsRare);
+
+            verticalLine.IsInteractive = true;
+        }
+
+        public override void Calculate(int index)
+        {
+        }
+    }
+}
