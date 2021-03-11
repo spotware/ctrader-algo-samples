784e3bc347a2fb4b4c8efe326fb31115fa98d928
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-03-11 11:06:49 +0300

    Added chart text sample

---
+++ Indicators/Chart Text Sample/Chart Text Sample/Chart Text Sample.cs
@@ -0,0 +1,42 @@
+﻿using cAlgo.API;
+
+namespace cAlgo
+{
+    /// <summary>
+    /// Thios sample shows how to use Chart.DrawText method to draw text
+    /// </summary>
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class ChartTextSample : Indicator
+    {
+        protected override void Initialize()
+        {
+            for (int iBarIndex = Chart.FirstVisibleBarIndex; iBarIndex <= Chart.LastVisibleBarIndex; iBarIndex++)
+            {
+                string text;
+
+                double y;
+
+                Color color;
+
+                if (Bars.ClosePrices[iBarIndex] > Bars.OpenPrices[iBarIndex])
+                {
+                    text = "U";
+                    y = Bars.LowPrices[iBarIndex];
+                    color = Color.Green;
+                }
+                else
+                {
+                    text = "D";
+                    y = Bars.HighPrices[iBarIndex];
+                    color = Color.Red;
+                }
+
+                Chart.DrawText("Text_" + iBarIndex, text, iBarIndex, y, color);
+            }
+        }
+
+        public override void Calculate(int index)
+        {
+        }
+    }
+}
