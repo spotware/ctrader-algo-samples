0fafabc4d7cffe18eb32ecd5ba92b0ef9fd37a8a
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-03-11 11:25:17 +0300

    Added Chart Trend line sample

---
+++ Indicators/Chart Trend Line Sample/Chart Trend Line Sample/Chart Trend Line Sample.cs
@@ -0,0 +1,22 @@
+﻿using cAlgo.API;
+
+namespace cAlgo
+{
+    /// <summary>
+    /// This sample shows how to use Chart.DrawTrendLine to draw a trend line on chart
+    /// </summary>
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class ChartTrendLineSample : Indicator
+    {
+        protected override void Initialize()
+        {
+            var trendLine = Chart.DrawTrendLine("trendLine", Chart.FirstVisibleBarIndex, Bars.LowPrices[Chart.FirstVisibleBarIndex], Chart.LastVisibleBarIndex, Bars.HighPrices[Chart.LastVisibleBarIndex], Color.Red, 2, LineStyle.Dots);
+
+            trendLine.IsInteractive = true;
+        }
+
+        public override void Calculate(int index)
+        {
+        }
+    }
+}
