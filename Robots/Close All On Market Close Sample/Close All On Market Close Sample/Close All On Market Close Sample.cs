19842bc23fb5d6cba84309aff8f760ac7f194f87
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-03-29 14:36:10 +0300

    Added close all on market close sample cBot

---
+++ Robots/Close All On Market Close Sample/Close All On Market Close Sample/Close All On Market Close Sample.cs
@@ -0,0 +1,42 @@
+﻿using cAlgo.API;
+using System;
+using System.Globalization;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This cBot closes all open positions before market close
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class CloseAllOnMarketCloseSample : Robot
+    {
+        private TimeSpan _closeBeforeTime;
+
+        [Parameter("Close Before", DefaultValue = "00:05:00")]
+        public string CloseBeforeTime { get; set; }
+
+        protected override void OnStart()
+        {
+            if (!TimeSpan.TryParse(CloseBeforeTime, CultureInfo.InvariantCulture, out _closeBeforeTime))
+            {
+                Print("You have provided invalid value for Close Before parameter");
+
+                Stop();
+            }
+
+            Timer.Start(1);
+        }
+
+        protected override void OnTimer()
+        {
+            var timeTillClose = Symbol.MarketHours.TimeTillClose();
+
+            if (!Symbol.MarketHours.IsOpened() || timeTillClose > _closeBeforeTime) return;
+
+            foreach (var position in Positions)
+            {
+                ClosePosition(position);
+            }
+        }
+    }
+}
