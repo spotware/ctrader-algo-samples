e7541eca8224e246b20cce5d1b90ebe7d6864232
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-04-02 16:35:41 +0300

    Added new samples

---
+++ Robots/Position Events Sample/Position Events Sample/Position Events Sample.cs
@@ -0,0 +1,35 @@
+﻿using cAlgo.API;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample shows how to handle position events
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class PositionEventsSample : Robot
+    {
+        protected override void OnStart()
+        {
+            Positions.Opened += Positions_Opened;
+            Positions.Closed += Positions_Closed;
+            Positions.Modified += Positions_Modified;
+        }
+
+        private void Positions_Modified(PositionModifiedEventArgs obj)
+        {
+            var modifiedPosition = obj.Position;
+        }
+
+        private void Positions_Closed(PositionClosedEventArgs obj)
+        {
+            var closedPosition = obj.Position;
+
+            var closeReason = obj.Reason;
+        }
+
+        private void Positions_Opened(PositionOpenedEventArgs obj)
+        {
+            var openedPosition = obj.Position;
+        }
+    }
+}
---
+++ Robots/Position Execution Sample/Position Execution Sample/Position Execution Sample.cs
@@ -0,0 +1,68 @@
+﻿using cAlgo.API;
+using cAlgo.API.Internals;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample shows how to execute a position or market order
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class PositionExecutionSample : Robot
+    {
+        [Parameter("Direction", DefaultValue = TradeType.Buy)]
+        public TradeType Direction { get; set; }
+
+        [Parameter("Volume (Lots)", DefaultValue = 0.01)]
+        public double VolumeInLots { get; set; }
+
+        [Parameter("Distance (Pips)", DefaultValue = 20, MinValue = 1)]
+        public double DistanceInPips { get; set; }
+
+        [Parameter("Stop (Pips)", DefaultValue = 10, MinValue = 0)]
+        public double StopInPips { get; set; }
+
+        [Parameter("Target (Pips)", DefaultValue = 10, MinValue = 0)]
+        public double TargetInPips { get; set; }
+
+        [Parameter("Label")]
+        public string Label { get; set; }
+
+        [Parameter("Comment")]
+        public string Comment { get; set; }
+
+        [Parameter("Trailing Stop", DefaultValue = false)]
+        public bool HasTrailingStop { get; set; }
+
+        [Parameter("Stop Loss Trigger Method", DefaultValue = StopTriggerMethod.Trade)]
+        public StopTriggerMethod StopLossTriggerMethod { get; set; }
+
+        [Parameter("Async", DefaultValue = false)]
+        public bool IsAsync { get; set; }
+
+        protected override void OnStart()
+        {
+            var volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+
+            DistanceInPips *= Symbol.PipSize;
+
+            var stopLoss = StopInPips == 0 ? null : (double?)StopInPips;
+            var takeProfit = TargetInPips == 0 ? null : (double?)TargetInPips;
+
+            TradeResult result = null;
+
+            if (IsAsync)
+                ExecuteMarketOrderAsync(Direction, SymbolName, volumeInUnits, Label, stopLoss, takeProfit, Comment, HasTrailingStop, StopLossTriggerMethod, OnCompleted);
+            else
+                result = ExecuteMarketOrder(Direction, SymbolName, volumeInUnits, Label, stopLoss, takeProfit, Comment, HasTrailingStop, StopLossTriggerMethod);
+
+            if (!IsAsync) OnCompleted(result);
+        }
+
+        private void OnCompleted(TradeResult result)
+        {
+            if (!result.IsSuccessful) Print("Error: ", result.Error);
+
+            Stop();
+        }
+    }
+}
