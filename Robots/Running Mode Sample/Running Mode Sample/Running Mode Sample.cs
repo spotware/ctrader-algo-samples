e70bc5563aa81aac5528bed903e73a4e6fc7c42c
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-03 13:41:45 +0300

    Added Running Mode Sample

---
+++ Robots/Running Mode Sample/Running Mode Sample/Running Mode Sample.cs
@@ -0,0 +1,33 @@
+﻿using cAlgo.API;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample shows how to use the RunningMode property of your robot
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class RunningModeSample : Robot
+    {
+        protected override void OnStart()
+        {
+            switch (RunningMode)
+            {
+                case RunningMode.RealTime:
+                    // If the robot is running on real time market condition
+                    break;
+
+                case RunningMode.SilentBacktesting:
+                    // If the robot is running on backtest and the visual mode is off
+                    break;
+
+                case RunningMode.VisualBacktesting:
+                    // If the robot is running on backtest and the visual mode is on
+                    break;
+
+                case RunningMode.Optimization:
+                    // If the robot is running on optimizer
+                    break;
+            }
+        }
+    }
+}
