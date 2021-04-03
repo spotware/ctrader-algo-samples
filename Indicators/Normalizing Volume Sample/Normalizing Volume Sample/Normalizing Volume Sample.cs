b7eb6df79c94fcfc8048ce98905fd03a496e1933
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-03 13:38:01 +0300

    Added Normalizing Volume Sample

---
+++ Indicators/Normalizing Volume Sample/Normalizing Volume Sample/Normalizing Volume Sample.cs
@@ -0,0 +1,39 @@
+﻿using cAlgo.API;
+
+namespace cAlgo
+{
+    /// <summary>
+    /// This sample shows how to normalize volume based on different rounding modes
+    /// </summary>
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class NormalizingVolumeSample : Indicator
+    {
+        [Parameter("Volume Unit", DefaultValue = VolumeUnit.Units)]
+        public VolumeUnit VolumeUnit { get; set; }
+
+        [Parameter("Volume Amount", DefaultValue = 0.01)]
+        public double VolumeAmount { get; set; }
+
+        [Parameter("Rounding Mode", DefaultValue = RoundingMode.ToNearest)]
+        public RoundingMode RoundingMode { get; set; }
+
+        protected override void Initialize()
+        {
+            double volumeInUnits = VolumeUnit == VolumeUnit.Units ? VolumeAmount : Symbol.QuantityToVolumeInUnits(VolumeAmount);
+
+            double normalizedVolume = Symbol.NormalizeVolumeInUnits(volumeInUnits, RoundingMode);
+
+            Print(normalizedVolume);
+        }
+
+        public override void Calculate(int index)
+        {
+        }
+    }
+
+    public enum VolumeUnit
+    {
+        Units,
+        Lots
+    }
+}
