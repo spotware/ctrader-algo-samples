0d92c63cceca187068f228c8669a22442fd8dc5d
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-07 13:49:34 +0300

    Added textbox sample

---
+++ Indicators/TextBox Sample/TextBox Sample/TextBox Sample.cs
@@ -0,0 +1,38 @@
+﻿using cAlgo.API;
+
+namespace cAlgo
+{
+    /// <summary>
+    /// This sample indicator shows how to add a text box control on your chart
+    /// </summary>
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class TextBoxSample : Indicator
+    {
+        protected override void Initialize()
+        {
+            var stackPanel = new StackPanel
+            {
+                BackgroundColor = Color.Gold,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Opacity = 0.6,
+            };
+
+            stackPanel.AddChild(new TextBox
+            {
+                Text = "Enter text here...",
+                FontWeight = FontWeight.ExtraBold,
+                Margin = 5,
+                ForegroundColor = Color.White,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Width = 150
+            });
+
+            Chart.AddControl(stackPanel);
+        }
+
+        public override void Calculate(int index)
+        {
+        }
+    }
+}
