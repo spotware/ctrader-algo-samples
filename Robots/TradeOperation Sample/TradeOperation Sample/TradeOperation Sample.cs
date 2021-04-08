3e456a7cb9f881f777a84e4cb19c30f615d3f627
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-08 13:25:50 +0300

    Added TradeOperation Sample

---
+++ Robots/TradeOperation Sample/TradeOperation Sample/TradeOperation Sample.cs
@@ -0,0 +1,30 @@
+﻿using cAlgo.API;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample cBot shows how to use TradeOperation to monitor an async order execution/placement operation
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class TradeOperationSample : Robot
+    {
+        protected override void OnStart()
+        {
+            var tradeOperation = ExecuteMarketOrderAsync(TradeType.Buy, SymbolName, Symbol.VolumeInUnitsMin, OnTradeResult);
+
+            if (tradeOperation.IsExecuting)
+            {
+                Print("Executing");
+            }
+            else
+            {
+                Print("Completed");
+            }
+        }
+
+        private void OnTradeResult(TradeResult obj)
+        {
+            Print("Was Trade Operation Successful: ", obj.IsSuccessful);
+        }
+    }
+}
