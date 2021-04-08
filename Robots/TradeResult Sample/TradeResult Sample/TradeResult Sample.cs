54d380483c1647526c23a8cb7e991d8f9c7a220f
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-08 13:36:22 +0300

    Added TradeResult Sample

---
+++ Robots/TradeResult Sample/TradeResult Sample/TradeResult Sample.cs
@@ -0,0 +1,26 @@
+﻿using cAlgo.API;
+
+namespace cAlgo.Robots
+{
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class TradeResultSample : Robot
+    {
+        protected override void OnStart()
+        {
+            var tradeResult = ExecuteMarketOrder(TradeType.Buy, SymbolName, Symbol.VolumeInUnitsMin);
+
+            if (tradeResult.IsSuccessful)
+            {
+                Print("Market order execution was successful");
+
+                var position = tradeResult.Position;
+
+                Print("A new position opend with ID ", position.Id);
+            }
+            else
+            {
+                Print("Market order execution was not successful");
+            }
+        }
+    }
+}
