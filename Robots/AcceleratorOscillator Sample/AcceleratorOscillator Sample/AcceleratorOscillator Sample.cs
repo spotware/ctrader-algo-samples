20f1220db4650cb688a8f9125f600a0cf0b87151
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-09 11:45:40 +0300

    Added AcceleratorOscillator Sample

---
+++ Robots/AcceleratorOscillator Sample/AcceleratorOscillator Sample/AcceleratorOscillator Sample.cs
@@ -0,0 +1,64 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample cBot shows how to use an Accelerator Oscillator indicator
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class AcceleratorOscillatorSample : Robot
+    {
+        private double _volumeInUnits;
+
+        private AcceleratorOscillator _acceleratorOscillator;
+
+        [Parameter("Volume (Lots)", DefaultValue = 0.01)]
+        public double VolumeInLots { get; set; }
+
+        [Parameter("Stop Loss (Pips)", DefaultValue = 10)]
+        public double StopLossInPips { get; set; }
+
+        [Parameter("Take Profit (Pips)", DefaultValue = 10)]
+        public double TakeProfitInPips { get; set; }
+
+        [Parameter("Label", DefaultValue = "AcceleratorOscillatorSample")]
+        public string Label { get; set; }
+
+        public Position[] BotPositions
+        {
+            get
+            {
+                return Positions.FindAll(Label);
+            }
+        }
+
+        protected override void OnStart()
+        {
+            _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+
+            _acceleratorOscillator = Indicators.AcceleratorOscillator();
+        }
+
+        protected override void OnBar()
+        {
+            foreach (var position in BotPositions)
+            {
+                if ((position.TradeType == TradeType.Buy && _acceleratorOscillator.Result.Last(1) < _acceleratorOscillator.Result.Last(2))
+                    || (position.TradeType == TradeType.Sell && _acceleratorOscillator.Result.Last(1) > _acceleratorOscillator.Result.Last(2)))
+                {
+                    ClosePosition(position);
+                }
+            }
+
+            if (_acceleratorOscillator.Result.Last(1) > 0 && _acceleratorOscillator.Result.Last(2) <= 0)
+            {
+                ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+            else if (_acceleratorOscillator.Result.Last(1) < 0 && _acceleratorOscillator.Result.Last(2) >= 0)
+            {
+                ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+        }
+    }
+}
