bef8be560419f2634d91c3e471af5c43e0f14ba5
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-10 13:03:29 +0300

    Added Commodity Channel Index Sample

---
+++ Robots/Commodity Channel Index Sample/Commodity Channel Index Sample/Commodity Channel Index Sample.cs
@@ -0,0 +1,69 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample cBot shows how to use the Chaikin Money Flow indicator
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class CommodityChannelIndexSample : Robot
+    {
+        private double _volumeInUnits;
+
+        private CommodityChannelIndex _commodityChannelIndex;
+
+        [Parameter("Volume (Lots)", DefaultValue = 0.01)]
+        public double VolumeInLots { get; set; }
+
+        [Parameter("Stop Loss (Pips)", DefaultValue = 10)]
+        public double StopLossInPips { get; set; }
+
+        [Parameter("Take Profit (Pips)", DefaultValue = 10)]
+        public double TakeProfitInPips { get; set; }
+
+        [Parameter("Label", DefaultValue = "Sample")]
+        public string Label { get; set; }
+
+        public Position[] BotPositions
+        {
+            get
+            {
+                return Positions.FindAll(Label);
+            }
+        }
+
+        protected override void OnStart()
+        {
+            _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+
+            _commodityChannelIndex = Indicators.CommodityChannelIndex(14);
+        }
+
+        protected override void OnBar()
+        {
+            if (_commodityChannelIndex.Result.Last(1) > 100 && _commodityChannelIndex.Result.Last(2) <= 100)
+            {
+                ClosePositions(TradeType.Sell);
+
+                ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+            else if (_commodityChannelIndex.Result.Last(1) < -100 && _commodityChannelIndex.Result.Last(2) >= -100)
+            {
+                ClosePositions(TradeType.Buy);
+
+                ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+        }
+
+        private void ClosePositions(TradeType tradeType)
+        {
+            foreach (var position in BotPositions)
+            {
+                if (position.TradeType != tradeType) continue;
+
+                ClosePosition(position);
+            }
+        }
+    }
+}
