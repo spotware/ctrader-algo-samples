5f4b215c625b712df7812fe225fe24722cdcea3c
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-12 15:37:43 +0300

    Added Linear Regression Intercept Sample

---
+++ Robots/Linear Regression Intercept Sample/Linear Regression Intercept Sample/Linear Regression Intercept Sample.cs
@@ -0,0 +1,67 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
+
+namespace cAlgo.Robots
+{
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class LinearRegressionInterceptSample : Robot
+    {
+        private double _volumeInUnits;
+
+        private LinearRegressionIntercept _linearRegressionIntercept;
+
+        [Parameter("Volume (Lots)", DefaultValue = 0.01)]
+        public double VolumeInLots { get; set; }
+
+        [Parameter("Stop Loss (Pips)", DefaultValue = 10)]
+        public double StopLossInPips { get; set; }
+
+        [Parameter("Take Profit (Pips)", DefaultValue = 10)]
+        public double TakeProfitInPips { get; set; }
+
+        [Parameter("Label", DefaultValue = "Sample")]
+        public string Label { get; set; }
+
+        public Position[] BotPositions
+        {
+            get
+            {
+                return Positions.FindAll(Label);
+            }
+        }
+
+        protected override void OnStart()
+        {
+            _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+
+            _linearRegressionIntercept = Indicators.LinearRegressionIntercept(Bars.ClosePrices, 20);
+        }
+
+        protected override void OnBar()
+        {
+            if (Bars.ClosePrices.Last(1) > _linearRegressionIntercept.Result.Last(1) && Bars.ClosePrices.Last(2) <= _linearRegressionIntercept.Result.Last(2))
+            {
+                ClosePositions(TradeType.Sell);
+
+                ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+            else if (Bars.ClosePrices.Last(1) < _linearRegressionIntercept.Result.Last(1) && Bars.ClosePrices.Last(2) >= _linearRegressionIntercept.Result.Last(2))
+            {
+                ClosePositions(TradeType.Buy);
+
+                ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+        }
+
+        private void ClosePositions(TradeType tradeType)
+        {
+            foreach (var position in BotPositions)
+            {
+                if (position.TradeType != tradeType) continue;
+
+                ClosePosition(position);
+            }
+        }
+    }
+}
