83064a7a7315551804a982306e3fa0db74e79c56
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-15 11:14:01 +0300

    Added Median Price Sample

---
+++ Robots/Median Price Sample/Median Price Sample/Median Price Sample.cs
@@ -0,0 +1,75 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample cBot shows how to use the Median Price indicator
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class MedianPriceSample : Robot
+    {
+        private double _volumeInUnits;
+
+        private MedianPrice _medianPrice;
+
+        [Parameter("Volume (Lots)", DefaultValue = 0.01)]
+        public double VolumeInLots { get; set; }
+
+        [Parameter("Stop Loss (Pips)", DefaultValue = 10)]
+        public double StopLossInPips { get; set; }
+
+        [Parameter("Take Profit (Pips)", DefaultValue = 10)]
+        public double TakeProfitInPips { get; set; }
+
+        [Parameter("Label", DefaultValue = "Sample")]
+        public string Label { get; set; }
+
+        public Position[] BotPositions
+        {
+            get
+            {
+                return Positions.FindAll(Label);
+            }
+        }
+
+        protected override void OnStart()
+        {
+            _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+
+            _medianPrice = Indicators.MedianPrice();
+        }
+
+        protected override void OnBar()
+        {
+            if (Bars.ClosePrices.Last(1) > _medianPrice.Result.Last(1))
+            {
+                ClosePositions(TradeType.Sell);
+
+                if (BotPositions.Length == 0)
+                {
+                    ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+                }
+            }
+            else if (Bars.ClosePrices.Last(1) < _medianPrice.Result.Last(1))
+            {
+                ClosePositions(TradeType.Buy);
+
+                if (BotPositions.Length == 0)
+                {
+                    ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+                }
+            }
+        }
+
+        private void ClosePositions(TradeType tradeType)
+        {
+            foreach (var position in BotPositions)
+            {
+                if (position.TradeType != tradeType) continue;
+
+                ClosePosition(position);
+            }
+        }
+    }
+}
