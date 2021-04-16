24547e971e56729f98f1f4ffaf3b7fe148dbee56
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-16 15:19:01 +0300

    Added Swing Index

---
+++ Robots/Swing Index Sample/Swing Index Sample/Swing Index Sample.cs
@@ -0,0 +1,69 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample cBot shows how to use the Swing Index indicator
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class SwingIndexSample : Robot
+    {
+        private double _volumeInUnits;
+
+        private SwingIndex _swingIndex;
+
+        [Parameter("Volume (Lots)", DefaultValue = 0.01)]
+        public double VolumeInLots { get; set; }
+
+        [Parameter("Stop Loss (Pips)", DefaultValue = 10)]
+        public double StopLossInPips { get; set; }
+
+        [Parameter("Take Profit (Pips)", DefaultValue = 10)]
+        public double TakeProfitInPips { get; set; }
+
+        [Parameter("Label", DefaultValue = "Sample")]
+        public string Label { get; set; }
+
+        public Position[] BotPositions
+        {
+            get
+            {
+                return Positions.FindAll(Label);
+            }
+        }
+
+        protected override void OnStart()
+        {
+            _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+
+            _swingIndex = Indicators.SwingIndex(12);
+        }
+
+        protected override void OnBar()
+        {
+            if (_swingIndex.Result.Last(1) > 0 && _swingIndex.Result.Last(2) <= 0)
+            {
+                ClosePositions(TradeType.Sell);
+
+                ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+            else if (_swingIndex.Result.Last(1) < 0 && _swingIndex.Result.Last(2) >= 0)
+            {
+                ClosePositions(TradeType.Buy);
+
+                ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+        }
+
+        private void ClosePositions(TradeType tradeType)
+        {
+            foreach (var position in BotPositions)
+            {
+                if (position.TradeType != tradeType) continue;
+
+                ClosePosition(position);
+            }
+        }
+    }
+}
