b8933d0bdc5d0f98deeb81799e1791f13f043989
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-16 13:56:41 +0300

    Added Rainbow Oscillator sample

---
+++ Robots/Rainbow Oscillator Sample/Rainbow Oscillator Sample/Rainbow Oscillator Sample.cs
@@ -0,0 +1,72 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample cBot shows how to use the Rainbow Oscillator indicator
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class RainbowOscillatorSample : Robot
+    {
+        private double _volumeInUnits;
+
+        private RainbowOscillator _rainbowOscillator;
+
+        private SimpleMovingAverage _simpleMovingAverage;
+
+        [Parameter("Volume (Lots)", DefaultValue = 0.01)]
+        public double VolumeInLots { get; set; }
+
+        [Parameter("Stop Loss (Pips)", DefaultValue = 10)]
+        public double StopLossInPips { get; set; }
+
+        [Parameter("Take Profit (Pips)", DefaultValue = 10)]
+        public double TakeProfitInPips { get; set; }
+
+        [Parameter("Label", DefaultValue = "Sample")]
+        public string Label { get; set; }
+
+        public Position[] BotPositions
+        {
+            get
+            {
+                return Positions.FindAll(Label);
+            }
+        }
+
+        protected override void OnStart()
+        {
+            _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+
+            _rainbowOscillator = Indicators.RainbowOscillator(Bars.ClosePrices, 9, MovingAverageType.Simple);
+            _simpleMovingAverage = Indicators.SimpleMovingAverage(_rainbowOscillator.Result, 9);
+        }
+
+        protected override void OnBar()
+        {
+            if (_rainbowOscillator.Result.HasCrossedAbove(_simpleMovingAverage.Result, 0))
+            {
+                ClosePositions(TradeType.Buy);
+
+                ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+            else if (_rainbowOscillator.Result.HasCrossedBelow(_simpleMovingAverage.Result, 0))
+            {
+                ClosePositions(TradeType.Sell);
+
+                ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+        }
+
+        private void ClosePositions(TradeType tradeType)
+        {
+            foreach (var position in BotPositions)
+            {
+                if (position.TradeType != tradeType) continue;
+
+                ClosePosition(position);
+            }
+        }
+    }
+}
