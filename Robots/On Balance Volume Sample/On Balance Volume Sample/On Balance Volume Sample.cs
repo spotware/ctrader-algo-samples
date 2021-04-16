ba06769b21bd9996153bd6008e2d590ea2521e02
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-16 13:39:53 +0300

    Added On Balance Volume sample

---
+++ Robots/On Balance Volume Sample/On Balance Volume Sample/On Balance Volume Sample.cs
@@ -0,0 +1,72 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample cBot shows how to use the On Balance Volume indicator
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class OnBalanceVolumeSample : Robot
+    {
+        private double _volumeInUnits;
+
+        private OnBalanceVolume _onBalanceVolume;
+
+        private SimpleMovingAverage _simpleMovingAverage;
+
+        [Parameter("Volume (Lots)", DefaultValue = 0.01)]
+        public double VolumeInLots { get; set; }
+
+        [Parameter("Stop Loss (Pips)", DefaultValue = 10)]
+        public double StopLossInPips { get; set; }
+
+        [Parameter("Take Profit (Pips)", DefaultValue = 10)]
+        public double TakeProfitInPips { get; set; }
+
+        [Parameter("Label", DefaultValue = "Sample")]
+        public string Label { get; set; }
+
+        public Position[] BotPositions
+        {
+            get
+            {
+                return Positions.FindAll(Label);
+            }
+        }
+
+        protected override void OnStart()
+        {
+            _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+
+            _onBalanceVolume = Indicators.OnBalanceVolume(Bars.ClosePrices);
+            _simpleMovingAverage = Indicators.SimpleMovingAverage(_onBalanceVolume.Result, 9);
+        }
+
+        protected override void OnBar()
+        {
+            if (_onBalanceVolume.Result.HasCrossedAbove(_simpleMovingAverage.Result, 0))
+            {
+                ClosePositions(TradeType.Sell);
+
+                ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+            else if (_onBalanceVolume.Result.HasCrossedBelow(_simpleMovingAverage.Result, 0))
+            {
+                ClosePositions(TradeType.Buy);
+
+                ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+        }
+
+        private void ClosePositions(TradeType tradeType)
+        {
+            foreach (var position in BotPositions)
+            {
+                if (position.TradeType != tradeType) continue;
+
+                ClosePosition(position);
+            }
+        }
+    }
+}
