f58ab1279f808e70261525e41e6468e5b6b02f0e
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-18 11:54:00 +0300

    Added Williams % R sample

---
+++ Robots/Williams % R Sample/Williams % R Sample/Williams % R Sample.cs
@@ -0,0 +1,69 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample cBot shows how to use the Williams % R indicator
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class WilliamsPctRSample : Robot
+    {
+        private double _volumeInUnits;
+
+        private WilliamsPctR _williamsPctR;
+
+        [Parameter("Volume (Lots)", DefaultValue = 0.01)]
+        public double VolumeInLots { get; set; }
+
+        [Parameter("Stop Loss (Pips)", DefaultValue = 10)]
+        public double StopLossInPips { get; set; }
+
+        [Parameter("Take Profit (Pips)", DefaultValue = 10)]
+        public double TakeProfitInPips { get; set; }
+
+        [Parameter("Label", DefaultValue = "Sample")]
+        public string Label { get; set; }
+
+        public Position[] BotPositions
+        {
+            get
+            {
+                return Positions.FindAll(Label);
+            }
+        }
+
+        protected override void OnStart()
+        {
+            _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+
+            _williamsPctR = Indicators.WilliamsPctR(14);
+        }
+
+        protected override void OnBar()
+        {
+            if (_williamsPctR.Result.Last(1) > -20 && _williamsPctR.Result.Last(2) < -20)
+            {
+                ClosePositions(TradeType.Buy);
+
+                ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+            else if (_williamsPctR.Result.Last(1) < -80 && _williamsPctR.Result.Last(2) > -80)
+            {
+                ClosePositions(TradeType.Sell);
+
+                ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+        }
+
+        private void ClosePositions(TradeType tradeType)
+        {
+            foreach (var position in BotPositions)
+            {
+                if (position.TradeType != tradeType) continue;
+
+                ClosePosition(position);
+            }
+        }
+    }
+}
