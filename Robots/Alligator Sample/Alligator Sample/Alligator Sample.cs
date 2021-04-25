6f6ac8c727846a586d22bf040d88ac7cec6a1933
Ahmad Noman Musleh <8859902+afhacker@users.noreply.github.com>
2021-04-25 15:11:17 +0300

    Added Alligator sample

---
+++ Robots/Alligator Sample/Alligator Sample/Alligator Sample.cs
@@ -0,0 +1,69 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample cBot shows how to use the Alligator indicator
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class AlligatorSample : Robot
+    {
+        private double _volumeInUnits;
+
+        private Alligator _alligator;
+
+        [Parameter("Volume (Lots)", DefaultValue = 0.01)]
+        public double VolumeInLots { get; set; }
+
+        [Parameter("Stop Loss (Pips)", DefaultValue = 10)]
+        public double StopLossInPips { get; set; }
+
+        [Parameter("Take Profit (Pips)", DefaultValue = 10)]
+        public double TakeProfitInPips { get; set; }
+
+        [Parameter("Label", DefaultValue = "Sample")]
+        public string Label { get; set; }
+
+        public Position[] BotPositions
+        {
+            get
+            {
+                return Positions.FindAll(Label);
+            }
+        }
+
+        protected override void OnStart()
+        {
+            _volumeInUnits = Symbol.QuantityToVolumeInUnits(VolumeInLots);
+
+            _alligator = Indicators.Alligator(13, 18, 8, 5, 5, 3);
+        }
+
+        protected override void OnBar()
+        {
+            if (_alligator.Lips.Last(1) > _alligator.Teeth.Last(1) && _alligator.Lips.Last(2) <= _alligator.Teeth.Last(2))
+            {
+                ClosePositions(TradeType.Sell);
+
+                ExecuteMarketOrder(TradeType.Buy, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+            else if (_alligator.Lips.Last(1) < _alligator.Teeth.Last(1) && _alligator.Lips.Last(2) >= _alligator.Teeth.Last(2))
+            {
+                ClosePositions(TradeType.Buy);
+
+                ExecuteMarketOrder(TradeType.Sell, SymbolName, _volumeInUnits, Label, StopLossInPips, TakeProfitInPips);
+            }
+        }
+
+        private void ClosePositions(TradeType tradeType)
+        {
+            foreach (var position in BotPositions)
+            {
+                if (position.TradeType != tradeType) continue;
+
+                ClosePosition(position);
+            }
+        }
+    }
+}
