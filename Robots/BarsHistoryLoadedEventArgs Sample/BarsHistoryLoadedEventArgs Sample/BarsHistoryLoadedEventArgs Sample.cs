960096334b4065278ee92603e0e4d3a2c26e9e94
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2022-01-17 11:55:52 +0300

    Added BarsHistoryLoadedEventArgs Sample

---
+++ Robots/BarsHistoryLoadedEventArgs Sample/BarsHistoryLoadedEventArgs Sample/BarsHistoryLoadedEventArgs Sample.cs
@@ -0,0 +1,29 @@
+﻿using cAlgo.API;
+
+namespace cAlgo.Robots
+{
+    /// <summary>
+    /// This sample indicator shows how to use BarsHistoryLoadedEventArgs
+    /// </summary>
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class BarsHistoryLoadedEventArgsSample : Robot
+    {
+        protected override void OnStart()
+        {
+            Bars.HistoryLoaded += Bars_HistoryLoaded;
+
+            // You can load more bars by calling this method or LoadMoreHistory
+            Bars.LoadMoreHistoryAsync();
+        }
+
+        // This method will be called if you scroll left on your chart and load more data
+        // Or if you call the Bars.LoadMoreHistory/LoadMoreHistoryAsync methods
+        // BarsHistoryLoadedEventArgs has two properties
+        // BarsHistoryLoadedEventArgs.Bars refers to the bars object that it's history is loaded
+        // BarsHistoryLoadedEventArgs.Count gives you the number of bars that has been loaded
+        private void Bars_HistoryLoaded(BarsHistoryLoadedEventArgs obj)
+        {
+            Print("Loaded Bars Count: {0}", obj.Count);
+        }
+    }
+}
