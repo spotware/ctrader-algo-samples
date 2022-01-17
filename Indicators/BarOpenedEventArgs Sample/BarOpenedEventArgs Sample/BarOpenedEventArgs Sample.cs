475db7113cae6739491bed621aa323701b13b54d
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2022-01-17 11:41:30 +0300

    Added BarOpenedEventArgs sample

---
+++ Indicators/BarOpenedEventArgs Sample/BarOpenedEventArgs Sample/BarOpenedEventArgs Sample.cs
@@ -0,0 +1,26 @@
+﻿using cAlgo.API;
+
+namespace cAlgo
+{
+    // This example shows how to use the Bars object BarOpened event BarOpenedEventArgs
+    [Indicator(IsOverlay = false, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class BarOpenedEventArgsSample : Indicator
+    {
+        protected override void Initialize()
+        {
+            Bars.BarOpened += Bars_BarOpened;
+        }
+
+        // This method will be called if a new bar opens
+        // BarOpenedEventArgs has a Bars property the you can use to get Bars object
+        private void Bars_BarOpened(BarOpenedEventArgs obj)
+        {
+            var newOpendBar = obj.Bars.LastBar; // Or you can use obj.Bars[Bars.Count - 1] or obj.Bars.Last(0)
+            var closedBar = obj.Bars.Last(1); // Or you can use obj.Bars[Bars.Count - 2]
+        }
+
+        public override void Calculate(int index)
+        {
+        }
+    }
+}
