39188bf27fb9cdba5cdd3b9d4f0705885665ce5a
evgenisedin <esedin@spotware.com>
2024-06-12 14:29:47 +0300

    my-ASP-example

---
+++ Plugins/My ASP Example/My ASP Example/My ASP Example.cs
@@ -0,0 +1,76 @@
+// -------------------------------------------------------------------------------------------------
+//
+//    This code is a cTrader Algo API example.
+//
+//    This code is intended to be used as a sample and does not guarantee any particular outcome or
+//    profit of any kind. Use it at your own risk.
+//    
+//    This example plugin adds a new section to the Active Symbol Panel (ASP), with styles applied to the plugin controls.
+//
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using cAlgo.API;
+using cAlgo.API.Collections;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
+
+namespace cAlgo.Plugins
+{
+    [Plugin(AccessRights = AccessRights.None)]
+    public class MyASPExample : Plugin
+    {
+    
+        TextBlock _txtBuyVWAP;
+        TextBlock _txtSellVWAP;
+        protected override void OnStart()
+        {
+            var block = Asp.SymbolTab.AddBlock("My ASP Example");
+            block.Index = 2;
+            block.Height = 100;
+            block.IsExpanded = true;
+            
+            var panel = new StackPanel
+            {
+                Orientation = Orientation.Vertical
+            };
+            
+            var textBoxStyle = new Style();
+            textBoxStyle.Set(ControlProperty.Width, 200);
+            textBoxStyle.Set(ControlProperty.Margin, 5);
+            textBoxStyle.Set(ControlProperty.FontFamily, "Cambria");
+            textBoxStyle.Set(ControlProperty.FontSize, 15);
+            
+            _txtBuyVWAP = new TextBlock
+            {
+                Text = "Buy Text Block",
+                Style = textBoxStyle,
+                ForegroundColor = Color.Green
+            };
+            
+            _txtSellVWAP = new TextBlock
+            {
+                Text = "Sell Text Block",
+                Style = textBoxStyle,
+                ForegroundColor = Color.Red
+            };
+            
+            panel.AddChild(_txtBuyVWAP);
+            panel.AddChild(_txtSellVWAP);
+            
+            block.Child = panel;
+            
+            var buyPositions = Positions.Where(p => p.TradeType == TradeType.Buy);
+            _txtBuyVWAP.Text = "Buy Positions VWAP: " + Math.Round((buyPositions.Sum(p => p.EntryPrice * p.VolumeInUnits)/ buyPositions.Sum(p => p.VolumeInUnits)), 5);
+            
+            var sellPositions = Positions.Where(p => p.TradeType == TradeType.Sell);
+            _txtSellVWAP.Text = "Sell Positions VWAP: " + Math.Round((sellPositions.Sum(p => p.EntryPrice * p.VolumeInUnits)/ sellPositions.Sum(p => p.VolumeInUnits)), 5);
+        }
+
+        protected override void OnStop()
+        {
+            // Handle Plugin stop here
+        }
+    }        
+}
