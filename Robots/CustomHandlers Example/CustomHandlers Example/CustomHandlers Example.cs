5eb45237ce27847e624f764e348a595ddcd754dc
sergikos7 <sisaev@spotware.com>
2025-03-27 10:47:29 +0200

    Update CustomHandlers Example.cs

--- Robots/CustomHandlers Example/CustomHandlers Example/CustomHandlers Example.cs
+++ Robots/CustomHandlers Example/CustomHandlers Example/CustomHandlers Example.cs
@@ -1,3 +1,16 @@
+// -------------------------------------------------------------------------------------------------
+//
+//    This code is a cTrader Algo API example.
+//
+//    This cBot is intended to be used as a sample and does not guarantee any particular outcome or
+//    profit of any kind. Use it at your own risk.
+//
+//    This sample cBot listens to bar opening events and performs trade operations based on
+//    simple bullish and bearish reversal patterns.
+//
+// -------------------------------------------------------------------------------------------------
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,30 +22,35 @@ using cAlgo.API.Internals;
 
 namespace cAlgo.Robots
 {
+    // Define the cBot attributes, such as access rights.
     [Robot(AccessRights = AccessRights.None)]
     public class CustomHandlersExample : Robot
     {
 
+        // This method is called once at the start of the cBot's execution.
         protected override void OnStart()
         {
-            Bars.BarOpened += BullishReversal;
-            Bars.BarOpened += BearishReversal;
+            Bars.BarOpened += BullishReversal;  // Add event handler for detecting bullish reversals.
+            Bars.BarOpened += BearishReversal;  // Adds event handler for detecting bearish reversals.
         }
 
-        
+        // Handler method to check and execute trades for a bullish reversal.        
         private void BullishReversal(BarOpenedEventArgs args) 
         {
+            // Checks for a bullish reversal condition based on previous bar closes.
             if (Bars.LastBar.Open > Bars.Last(1).Close && Bars.LastBar.Open > Bars.Last(2).Close) 
             {
-                ExecuteMarketOrder(TradeType.Buy, SymbolName, 10000, null, 10, 50);
+                ExecuteMarketOrder(TradeType.Buy, SymbolName, 10000, null, 10, 50);  // Places a buy market order if the bullish reversal condition is met.
             }
         }
-        
+
+        // Handler method to check and execute trades for a bearish reversal.
         private void BearishReversal(BarOpenedEventArgs args) 
         {
+            // Checks for a bearish reversal condition based on previous bar closes.
             if (Bars.LastBar.Open < Bars.Last(1).Close && Bars.LastBar.Open < Bars.Last(2).Close) 
             {
-                ExecuteMarketOrder(TradeType.Sell, SymbolName, 10000, null, 10, 50);
+                ExecuteMarketOrder(TradeType.Sell, SymbolName, 10000, null, 10, 50);  // Places a sell market order if the bearish reversal condition is met.
             }
         }
                 
