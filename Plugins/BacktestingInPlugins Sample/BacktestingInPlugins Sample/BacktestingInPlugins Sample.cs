9af38088c574018da3556c259ebc5b3a5f7110f1
sergikos7 <sisaev@spotware.com>
2025-03-27 11:25:39 +0200

    Update BacktestingInPlugins Sample.cs

--- Plugins/BacktestingInPlugins Sample/BacktestingInPlugins Sample/BacktestingInPlugins Sample.cs
+++ Plugins/BacktestingInPlugins Sample/BacktestingInPlugins Sample/BacktestingInPlugins Sample.cs
@@ -5,10 +5,10 @@
 //    This code is intended to be used as a sample and does not guarantee any particular outcome or
 //    profit of any kind. Use it at your own risk.
 //    
-//    This sample adds a new block into the ASP. The block contains a ComboBox, a custom Button,
-//    and a TextBlock. Upon choosing one of their installed cBots in the ComboBox, the user can click
-//    on the Button and backtest the chosen cBot on EURUSD h1. When backtesting is finished, the plugin
-//    will display its results in the TextBlock.
+//    The sample adds a new block into Active Symbol Panel (ASP). The block contains a ComboBox, 
+//    a custom Button and a TextBlock. Upon choosing one of their installed cBots in the ComboBox, 
+//    the user can click on the Button and backtest the chosen cBot on EURUSD h1. When backtesting 
+//    is finished, the plugin will display its results in the TextBlock.
 //
 // -------------------------------------------------------------------------------------------------
 
@@ -23,29 +23,33 @@ using System.Text.Json.Nodes;
 
 namespace cAlgo.Plugins
 {
+    // Declaring the class as a plugin without requiring special access permissions.
     [Plugin(AccessRights = AccessRights.None)]
     public class BacktestingInPluginsSample : Plugin
     {
-
-        // Declaring the necessary UI elements
-        // and the cBot (RobotType) selected in the ComboBox
+        // Declaring the necessary UI elements and the cBot (RobotType) selected in the ComboBox.
         private Grid _grid;
         private ComboBox _cBotsComboBox;
         private Button _startBacktestingButton;
         private TextBlock _resultsTextBlock;
         private RobotType _selectedRobotType;
 
+        // This method is triggered when the plugin starts.
         protected override void OnStart()
         {
-            // Initialising and structuring the UI elements
+            // Initialising and structuring the UI elements into a grid layout.
             _grid = new Grid(3, 1);
             _cBotsComboBox = new ComboBox();
+            
+            // Initialising a button for starting the backtesting process.
             _startBacktestingButton = new Button
             {
                 BackgroundColor = Color.Green,
                 CornerRadius = new CornerRadius(5),
                 Text = "Start Backtesting",
             };
+            
+            // Creating a TextBlock to display information.
             _resultsTextBlock = new TextBlock
             {
                 HorizontalAlignment = HorizontalAlignment.Center,
@@ -53,30 +57,28 @@ namespace cAlgo.Plugins
                 Text = "Select a cBot...",
             };
 
+            // Adding UI elements to the grid.
             _grid.AddChild(_cBotsComboBox, 0, 0);
             _grid.AddChild(_startBacktestingButton, 1, 0);
             _grid.AddChild(_resultsTextBlock, 2, 0);
-
            
+            // Adding a new block in the ASP with the grid as its content.
             var block = Asp.SymbolTab.AddBlock("Backtesting Plugin");
-
             block.Child = _grid;
             
-             // Populating the ComboBox with existing cBots
+             // Populating the ComboBox with existing cBots.
             PopulateCBotsComboBox();
 
-            // Assigning event handlers to the Button.Click,
-            // ComboBox.SelectedItemChanged, and Backtesting.Completed events
+            // Assigning event handlers to the Button.Click, ComboBox.SelectedItemChanged and Backtesting.Completed events.
             _startBacktestingButton.Click += StartBacktestingButton_Click;
             _cBotsComboBox.SelectedItemChanged += CBotsComboBox_SelectedItemChanged;
             Backtesting.Completed += Backtesting_Completed;
-
         }
         
+        // Event handler triggered when the backtesting button is clicked.
         protected void StartBacktestingButton_Click(ButtonClickEventArgs obj)
         {
-        
-            // Initialising and configuring the backtesting settings
+            // Initialising and configuring the backtesting settings.
             var backtestingSettings = new BacktestingSettings 
             {
                 DataMode = BacktestingDataMode.M1,
@@ -85,46 +87,46 @@ namespace cAlgo.Plugins
                 Balance = 10000,
             };
             
-            // Starting backtesting on EURUSD h1
+            // Starting backtesting on EURUSD h1.
             Backtesting.Start(_selectedRobotType, "EURUSD", TimeFrame.Hour, backtestingSettings);
             
-            // Disabling other controls and changing
-            // the text inside the TextBlock
+            // Disabling other controls and changing the text inside the TextBlock.
             _cBotsComboBox.IsEnabled = false;
             _startBacktestingButton.IsEnabled = false;
             _resultsTextBlock.Text = "Backtesting in progress...";
         }
 
+        // Populating the ComboBox with the names of all installed cBots available in the Algo Registry.
         protected void PopulateCBotsComboBox()
         {
-            // Iterating over the AlgoRegistry and
-            // getting the names of all installed cBots
+            // Iterating over the Algo Registry and getting the names of all installed cBots.
             foreach (var robotType in AlgoRegistry.OfType<RobotType>())
             {
                 _cBotsComboBox.AddItem(robotType.Name);
             }
         }
 
+        // Event handler triggered when backtesting is completed.
         protected void Backtesting_Completed(BacktestingCompletedEventArgs obj)
         {
-            // Attaining the JSON results of backtesting
+            // Attaining the JSON results of backtesting.
             string jsonResults = obj.JsonReport;
             
-            // Converting the JSON string into a JsonNode
+            // Converting the JSON string into a JsonNode.
             JsonNode resultsNode = JsonNode.Parse(jsonResults);
             
-            // Attaining the ROI and net profit from backtesting results
+            // Attaining the ROI and net profit from backtesting results.
             _resultsTextBlock.Text = $"ROI: {resultsNode["main"]["roi"]}\nNet Profit: {resultsNode["main"]["netProfit"]}";
             
-            // Re-enabling controls after backteting is finished
+            // Re-enabling controls after backteting is finished.
             _cBotsComboBox.IsEnabled = true;
             _startBacktestingButton.IsEnabled = true;
         }
 
+        // Event handler triggered when the ComboBox selection changes.
         protected void CBotsComboBox_SelectedItemChanged(ComboBoxSelectedItemChangedEventArgs obj)
         {
-            // Updading the variable to always contain
-            // the cBto selected in the ComboBox
+            // Updading the variable to always contain the cBot selected in the ComboBox.
             _selectedRobotType = AlgoRegistry.Get(obj.SelectedItem) as RobotType;
         }
 
