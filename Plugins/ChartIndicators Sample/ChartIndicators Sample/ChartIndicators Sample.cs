fbc322ec8d738aaf0a86e67ca978158e7b7abd0e
sergikos7 <sisaev@spotware.com>
2025-03-27 11:26:30 +0200

    Update ChartIndicators Sample.cs

--- Plugins/ChartIndicators Sample/ChartIndicators Sample/ChartIndicators Sample.cs
+++ Plugins/ChartIndicators Sample/ChartIndicators Sample/ChartIndicators Sample.cs
@@ -5,42 +5,61 @@
 //    This code is intended to be used as a sample and does not guarantee any particular outcome or
 //    profit of any kind. Use it at your own risk.
 //    
-//    This sample adds an active symbol panel tab, and uses ChartIndicators API to show stats about
-//    active chart indicators and lets you add and remove indicators to active chart.
+//    Using ChartManager, the plugin reacts to new charts being added or removed. On every
+//    such action, the plugin prints a message with the log containing the Id of the chart 
+//    that has just been added or removed.
 //
 // -------------------------------------------------------------------------------------------------
 
+using System;
 using cAlgo.API;
+using cAlgo.API.Collections;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
 
 namespace cAlgo.Plugins
 {
+    // Declaring the class as a plugin without requiring special access permissions.
     [Plugin(AccessRights = AccessRights.None)]
-    public class ChartIndicatorsSample : Plugin
+    public class ChartIdSample : Plugin
     {
-        private ChartIndicatorsControl _chartIndicatorsControl;
-        
         protected override void OnStart()
         {
-            var aspTab = Asp.AddTab("Chart Indicators");
-            
-            _chartIndicatorsControl = new ChartIndicatorsControl(AlgoRegistry)
-            {
-                VerticalAlignment = VerticalAlignment.Top
-            };
-
-            aspTab.Child = _chartIndicatorsControl;
-
-            SetControlChart();
-
-            ChartManager.ActiveFrameChanged += _ => SetControlChart();
+            // Assigning custom handlers for the ChartManager.FramesAdded and ChartManager.FramesRemoved events.
+            ChartManager.FramesAdded += ChartManager_FramesAdded;
+            ChartManager.FramesRemoved += ChartManager_FramesRemoved;
         }
 
-        private void SetControlChart()
+        private void ChartManager_FramesRemoved(FramesRemovedEventArgs obj)
         {
-            if (ChartManager.ActiveFrame is not ChartFrame chartFrame)
-                return;
-
-            _chartIndicatorsControl.Chart = chartFrame.Chart;
+            // Iterating over a collection of removed frames.
+            foreach(var frame in obj.RemovedFrames) 
+            {
+                // Checking if a removed frame is a ChartFrame.
+                if (frame is ChartFrame) 
+                {
+                    // Downcasting the frame to a ChartFrame and printing the message.
+                    var chartFrame = frame as ChartFrame;
+                    Print($"Chart {chartFrame.Chart.Id} removed");
+                }
+            }
         }
+
+        private void ChartManager_FramesAdded(FramesAddedEventArgs obj)
+        {
+            // Iterating over a collection of added frames.
+            foreach(var frame in obj.AddedFrames) 
+            {
+                // Checking if an added frame is a ChartFrame.
+                if (frame is ChartFrame) 
+                {
+                    // Downcasting the frame to a ChartFrame and printing the message.
+                    var chartFrame = frame as ChartFrame;
+                    Print($"Chart {chartFrame.Chart.Id} added");
+                }
+            }
+        }
+
+
     }        
 }
