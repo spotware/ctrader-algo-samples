3f47409b7112a0087e77770bfb6ad358add82c38
sergikos7 <sisaev@spotware.com>
2025-03-27 11:35:12 +0200

    Update SmoothMouseMove Sample.cs

--- Plugins/SmoothMouseMove Sample/SmoothMouseMove Sample/SmoothMouseMove Sample.cs
+++ Plugins/SmoothMouseMove Sample/SmoothMouseMove Sample/SmoothMouseMove Sample.cs
@@ -5,7 +5,7 @@
 //    This code is intended to be used as a sample and does not guarantee any particular outcome or
 //    profit of any kind. Use it at your own risk.
 //    
-//    This sample opens a new chart for GPBUSD on start. When the user hovers the mouse cursor over
+//    The sample opens a new chart for GPBUSD on start. When the user hovers the mouse cursor over
 //    this chart, the plugin shows a Border containing a TextBlock that demonstrates whether showing
 //    orders on the chart is enabled/disabled. When the user clicks on the chart, the displaying of
 //    orders is enabled/disabled with the text inside the TextBlock being updated automatically.
@@ -20,11 +20,11 @@ using cAlgo.API.Internals;
 
 namespace cAlgo.Plugins
 {
+    // Declaring the class as a plugin without requiring special access permissions.
     [Plugin(AccessRights = AccessRights.None)]
     public class SmoothMouseMoveSample : Plugin
     {
-        // Declaring the required controls and
-        // the variable for storing the Chart
+        // Declaring the required controls and the variable for storing the chart.
         private TextBlock _ordersStatusTextBlock;
         private Border _ordersStatusBorder;
         private Canvas _canvas;
@@ -32,12 +32,10 @@ namespace cAlgo.Plugins
         
         protected override void OnStart()
         {
-            // Opening a new chart for GBPUSD and
-            // storing the Chart inside a varaible
+            // Opening a new chart for GBPUSD and storing the chart inside a varaible.
             _gpbusdChart = ChartManager.AddChartFrame("GBPUSD", TimeFrame.Minute2).Chart;
             
-            // Initialising the Border and
-            // setting its parameters
+            // Initialising the border and setting its parameters.
             _ordersStatusBorder = new Border {
                 IsHitTestVisible = false,
                 CornerRadius = 3,
@@ -47,25 +45,23 @@ namespace cAlgo.Plugins
                 IsVisible = false,            
             };
 
-            // Initialising the TextBlock and
-            // settings its initial text
+            // Initialising the TextBlock and settings its initial text.
             _ordersStatusTextBlock = new TextBlock
             {
                 Text = _gpbusdChart.DisplaySettings.Orders.ToString()
             };
 
-            // Initialising the Canvas and
-            // placing the TextBlock inside it
+            // Initialising the canvas and placing the TextBlock inside it.
             _canvas = new Canvas();
             _canvas.AddChild(_ordersStatusBorder);
             
-            // Placing the TextBlock inside the Border
+            // Placing the TextBlock inside the border.
             _ordersStatusBorder.Child = _ordersStatusTextBlock;
 
-            // Adding the Canvas to the Chart
+            // Adding the canvas to the chart.
             _gpbusdChart.AddControl(_canvas);
             
-            // Handling various mouse events
+            // Handling various mouse events.
             _gpbusdChart.MouseUp += GpbusdChartOnMouseUp;
             _gpbusdChart.MouseEnter += GpbusdChartOnMouseEnter;
             _gpbusdChart.MouseLeave += GpbusdChartOnMouseLeave;
@@ -75,32 +71,29 @@ namespace cAlgo.Plugins
 
         private void GpbusdChartOnMouseMove(ChartMouseEventArgs obj)
         {
-            // Moving the border to follow the mouse cursor
+            // Moving the border to follow the mouse cursor.
             _ordersStatusBorder.Top = obj.MouseY;
             _ordersStatusBorder.Left = obj.MouseX;
         }
 
         private void GpbusdChartOnMouseLeave(ChartMouseEventArgs obj)
         {
-            // Hiding the border whenever the
-            // mouse cursor leaves the chart
+            // Hiding the border whenever the mouse cursor leaves the chart.
             _ordersStatusBorder.IsVisible = false;
         }
 
         private void GpbusdChartOnMouseEnter(ChartMouseEventArgs obj)
         {
-            // Showing the border whenever the
-            // mouse cursor enters the chart
+            // Showing the border whenever the mouse cursor enters the chart.
             _ordersStatusBorder.IsVisible = true;
         }
 
         private void GpbusdChartOnMouseUp(ChartMouseEventArgs obj)
         {
-            // Enabling or disabling the displaying of orders on the chart
+            // Enabling or disabling the displaying of orders on the chart.
             _gpbusdChart.DisplaySettings.Orders = !_gpbusdChart.DisplaySettings.Orders;
             
-            // Updating the text inside the TextBlock to match
-            // the current settings
+            // Updating the text inside the TextBlock to match the current settings.
             _ordersStatusTextBlock.Text = _gpbusdChart.DisplaySettings.Orders.ToString();
         }
     }        
