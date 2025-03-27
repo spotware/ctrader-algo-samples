c7b03366736a20b5daf0af18291ec77dc0f45cfb
sergikos7 <sisaev@spotware.com>
2025-03-27 11:29:03 +0200

    Update IndicatorTitles Sample.cs

--- Plugins/IndicatorTitles Sample/IndicatorTitles Sample/IndicatorTitles Sample.cs
+++ Plugins/IndicatorTitles Sample/IndicatorTitles Sample/IndicatorTitles Sample.cs
@@ -5,7 +5,7 @@
 //    This code is intended to be used as a sample and does not guarantee any particular outcome or
 //    profit of any kind. Use it at your own risk.
 //    
-//    This sample uses the Chart.DisplaySettings.IndicatorTitles property
+//    The sample uses the Chart.DisplaySettings.IndicatorTitles property
 //    and prints its value on initialisation and whenever the user changes
 //    the chart display options. This is done by first opening a new chart
 //    for EURUSD and then assigning a custom event handler for the DisplaySettingsChanged
@@ -21,24 +21,26 @@ using cAlgo.API.Internals;
 
 namespace cAlgo.Plugins
 {
+    // Declaring the class as a plugin without requiring special access permissions.
     [Plugin(AccessRights = AccessRights.None)]
     public class IndicatorTitlesSample : Plugin
     {
-        // Declaring a new Chart variable
+        // Declaring a new chart variable.
         Chart _newChart;
         
+        // This method is executed when the plugin starts.
         protected override void OnStart()
         {
-            // Creating a new Chart for EURUSD on plugin start
+            // Creating a new chart for EURUSD on plugin start.
             _newChart = ChartManager.AddChartFrame("EURUSD", TimeFrame.Daily).Chart;
             
-            // Adding a custom event handler for the DisplaySettingsChanged event
+            // Adding a custom event handler for the DisplaySettingsChanged event.
             _newChart.DisplaySettingsChanged += OnDisplaySettingsChanged;
 
             Print(_newChart.DisplaySettings.IndicatorTitles);
         }
         
-        // Defining the custom event handler for the DisplaySettingsChanged event
+        // Defining the custom event handler for the DisplaySettingsChanged event.
         protected void OnDisplaySettingsChanged(ChartDisplaySettingsEventArgs args)
         {
             Print(_newChart.DisplaySettings.IndicatorTitles);
